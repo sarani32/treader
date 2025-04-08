c8230ea34d435e6624f92179dd5cd0ef2f7906cd
sarani32 <scott.sarani@gmail.com>
2025-04-08 10:11:54 +0330

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# treader
+# AutoCryptoTraderBot
+A smart auto-trading bot for crypto (Binance & TheTrueTrade).
---
+++ main.py
@@ -0,0 +1,5 @@
+from bot.trading import TradingBot
+
+if __name__ == "__main__":
+    bot = TradingBot()
+    bot.run()
